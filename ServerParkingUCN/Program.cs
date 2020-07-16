00813af2b0abc4c346ab098e85ff52832b0fdd21
Bar011 <Bar011@alumnos.ucn.cl>
2020-07-15 23:25:53 -0400

    feat(Program.cs): Add modules

--- ServerParkingUCN/Program.cs
+++ ServerParkingUCN/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using ServerZeroIce;
+using ServerZeroIce.model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ServerParkingUCN
 {
@@ -6,7 +11,46 @@ namespace ServerParkingUCN
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+          public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host
+            .CreateDefaultBuilder(args)
+            // Development, Staging, Production
+            .UseEnvironment("Development")
+            // Logging configuration
+            .ConfigureLogging(logging => 
+            {
+                logging.ClearProviders();
+                logging.AddConsole(options => 
+                {
+                    options.IncludeScopes = true;
+                    options.TimestampFormat = "[yyyyMMdd.HHmmss.fff]";
+                    options.DisableColors = false;
+                });
+                logging.SetMinimumLevel(LogLevel.Trace);
+            })
+            // Enable Control+C listener
+            .UseConsoleLifetime()
+            // Service inside the DI
+            .ConfigureServices((hostContext, services) => 
+            {
+                // The ParkingContext
+                services.AddDbContext<ParkingUCNContext>(); 
+                //Singleton
+                //services.AddSingleton<ContratosDisp_, ContratosImpl>();
+                // The ServerParkingUcnService
+                services.AddHostedService<ParkingUCNService>();
+                
+                // The Logger
+                services.AddLogging();
+                // The wait for finish
+                services.Configure<HostOptions>(option => 
+                {
+                    option.ShutdownTimeout = System.TimeSpan.FromSeconds(15);
+                });
+            });
+        
     }
 }
