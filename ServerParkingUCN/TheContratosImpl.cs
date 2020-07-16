7cffdc9d2670a980f48ff9d06c11fcc16560364d
Bar011 <Bar011@alumnos.ucn.cl>
2020-07-15 23:28:41 -0400

    feat(TheContratosImpl.cs): Create class and add modules

---
+++ ServerParkingUCN/TheContratosImpl.cs
@@ -0,0 +1,13 @@
+using Ice;
+using ServerZeroIce.model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Dao; 
+
+namespace ServerParkingUCN
+{
+    public class TheContratosImpl
+    {
+        
+    }
+}
