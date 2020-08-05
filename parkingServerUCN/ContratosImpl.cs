5658db7430a37a616d67fac58d83577f4d0f39a6
Bar011 <Bar011@alumnos.ucn.cl>
2020-08-05 18:21:27 -0400

    fix(ContratosImpls.cs): bug fixed and move methods

--- parkingServerUCN/ContratosImpl.cs
+++ parkingServerUCN/ContratosImpl.cs
@@ -42,117 +42,6 @@ namespace ServerParkingUCN.ZeroIce
             _logger.LogDebug("Done.");
         }
 
-        ///Adds a Persona to Database
-        public override Persona registrarPersona(Persona persona, Current current = null)
-        {
-
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                pc.Personas.Add(persona);
-                pc.SaveChanges();
-                return persona;
-            }
-
-            throw new System.NotImplementedException();
-        }
-
-        // Adds a Vehiculo to Database
-        public override Vehiculo registrarVehiculo(Vehiculo vehiculo, Current current = null)
-        {
-
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                pc.Vehiculos.Add(vehiculo);
-                pc.SaveChanges();
-                return vehiculo;
-            }
-            throw new System.NotImplementedException();
-        }
-
-        // Given a patente, returns a vehiculo from Database
-        public override Vehiculo obtenerVehiculo(string patente, Current current)
-        {
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                Vehiculo vehiculo = pc.Vehiculos.Find(patente);
-                pc.SaveChanges();
-                return vehiculo;
-            }
-            throw new System.NotImplementedException();
-        }
-
-        // Given a rut, returns a persona from Database
-        public override Persona obtenerPersona(string rut, Current current)
-        {
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                Persona persona = pc.Personas.Find(rut);
-                pc.SaveChanges();
-                return persona;
-            }
-            throw new System.NotImplementedException();
-
-        }
-            //Given a patente, deleting a vehiculo from Database
-        public override Vehiculo eliminarVehiculo(string patente, Current current)
-        {
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                Vehiculo vehiculo = pc.Vehiculos.Find(patente);
-                pc.Vehiculos.Remove(vehiculo);
-                pc.SaveChanges();
-                return vehiculo;
-            }
-            throw new System.NotImplementedException();
-        }
-
-
-            //Given a rut, deleting a persona from Database
-        public override Persona eliminarPersona(string rut, Current current)
-        {
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                Persona persona = pc.Personas.Find(rut);
-                pc.Personas.Remove(persona);
-                pc.SaveChanges();
-                return persona;
-            }
-
-             throw new System.NotImplementedException();
-        }
-
-           //Editing a persona from database. 
-
-        public override Persona editarPersona(Persona persona, Current current)
-        {
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                pc.Personas.Update(persona);
-                pc.SaveChanges();
-                return persona;
-            }
-            throw new System.NotImplementedException();
-        }
-        
-        //Editing a Vehiculo from de database.
-        public override Vehiculo editarVehiculo(Vehiculo vehiculo, Current current)
-        {
-            using (var scope = _serviceScopeFactory.CreateScope())
-            {
-                ServerParkingUCNContext pc = scope.ServiceProvider.GetService<ServerParkingUCNContext>();
-                pc.Vehiculos.Update(vehiculo);
-                pc.SaveChanges();
-                return vehiculo;
-            }
-            throw new System.NotImplementedException();
-        }
 
         public override bool verificarPatenteLogo(string patente, string codigoLogo, Current current)
         {
@@ -168,7 +57,6 @@ namespace ServerParkingUCN.ZeroIce
             }
             throw new System.NotImplementedException();
         }
-
         public override bool verificarLogoPatente(string patente, string codigoLogo, Current current)
         {
             using (var scope = _serviceScopeFactory.CreateScope())
